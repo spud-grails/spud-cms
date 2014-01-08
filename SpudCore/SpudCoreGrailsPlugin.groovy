9e4c52a9a1e21d2eaab101754b1e8a4432d2862b
tbarker <mingus.karate@gmail.com>
2014-01-08 11:47:38 -0500

    cleanup spud core
    cleaned up BuildConfig
    added grails wrapper
    removed unused unit tests
    added .gitignore
    removed plugin files
    removed ide files
    removed unused script files

--- SpudCore/SpudCoreGrailsPlugin.groovy
+++ SpudCore/SpudCoreGrailsPlugin.groovy
@@ -1,79 +1,36 @@
-import org.springframework.beans.factory.support.*
-
 class SpudCoreGrailsPlugin {
-		// the plugin version
-		def groupId = "com.bertramlabs"
-		def version = "0.1-SNAPSHOT"
-		// the version or versions of Grails the plugin is designed for
-		def grailsVersion = "2.3 > *"
-		// resources that are excluded from plugin packaging
-		def pluginExcludes = [
-				"grails-app/views/error.gsp"
-		]
+    def groupId = "com.bertramlabs"
+    def version = "0.1-SNAPSHOT"
+    def grailsVersion = "2.3 > *"
+    def pluginExcludes = [
+            "grails-app/views/error.gsp"
+    ]
 
-		// TODO Fill in these fields
-		def title = "Spud Core Plugin" // Headline display name of the plugin
-		def author = "David Estes"
-		def authorEmail = "destes@bcap.com"
-		def description = '''\
-Brief summary/description of the plugin.
+    def title = "Spud Core Plugin" // Headline display name of the plugin
+    def author = "David Estes"
+    def authorEmail = "destes@bcap.com"
+    def description = '''\
+Provides core functionality to spud modules
 '''
 
-		// URL to the plugin's documentation
-		def documentation = "http://grails.org/plugin/spud-core"
+    def documentation = "http://grails.org/plugin/spud-core"
+    def license = "APACHE"
+    def organization = [name: "Bertram Labs", url: "http://www.bertramlabs.com/"]
+    def issueManagement = [system: "GITHUB", url: "https://github.com/bertramdev/spud-grails/issues"]
+    def scm = [url: "https://github.com/bertramdev/spud-grails"]
 
-		// Extra (optional) plugin metadata
+    def doWithSpring = {
+        def beanName = application.config.spud.securityService ? application.config.spud.securityService : 'abstractSpudSecurityService'
+        springConfig.addAlias "spudSecurity", beanName
 
-		// License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
+        application.config.spud.renderers = application.config.spud.renderers ?: [:]
+        application.config.spud.templateEngines = application.config.spud.templateEngines ?: [:]
+        application.config.spud.renderers.gsp = 'defaultSpudRendererService'
+        application.config.spud.templateEngines.system = 'defaultSpudTemplateService'
+    }
 
-		// Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
+    def doWithApplicationContext = { ctx ->
+        ctx.adminApplicationService.initialize()
 
-		// Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-		// Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-		// Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
-
-		def doWithWebDescriptor = { xml ->
-				// TODO Implement additions to web.xml (optional), this event occurs before
-		}
-
-		def doWithSpring = {
-			def beanName = application.config.spud.securityService ? application.config.spud.securityService : 'abstractSpudSecurityService'
-			springConfig.addAlias "spudSecurity", beanName
-
-			application.config.spud.renderers              = application.config.spud.renderers ?: [:]
-			application.config.spud.templateEngines        = application.config.spud.templateEngines ?: [:]
-			application.config.spud.renderers.gsp          = 'defaultSpudRendererService'
-			application.config.spud.templateEngines.system = 'defaultSpudTemplateService'
-		}
-
-		def doWithDynamicMethods = { ctx ->
-				// TODO Implement registering dynamic methods to classes (optional)
-		}
-
-		def doWithApplicationContext = { ctx ->
-			ctx.adminApplicationService.initialize()
-
-		}
-
-		def onChange = { event ->
-				// TODO Implement code that is executed when any artefact that this plugin is
-				// watching is modified and reloaded. The event contains: event.source,
-				// event.application, event.manager, event.ctx, and event.plugin.
-		}
-
-		def onConfigChange = { event ->
-				// TODO Implement code that is executed when the project configuration changes.
-				// The event is the same as for 'onChange'.
-		}
-
-		def onShutdown = { event ->
-				// TODO Implement code that is executed when the application shuts down (optional)
-		}
+    }
 }
