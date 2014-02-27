ebe835524286410e668ffb3f8e721ac729f6a324
David Estes <davydotcom@gmail.com>
2014-02-27 00:06:05 -0800

    fixing link nav

--- SpudCmsGrailsPlugin.groovy
+++ SpudCmsGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class SpudCmsGrailsPlugin {
-    def version = "0.1.1"
+    def version = "0.1.2"
     def grailsVersion = "2.3 > *"
     def pluginExcludes = [
         "grails-app/views/error.gsp"
--- grails-app/taglib/spud/cms/SpudCmsTagLib.groovy
+++ grails-app/taglib/spud/cms/SpudCmsTagLib.groovy
@@ -38,7 +38,7 @@ class SpudCmsTagLib {
 			def active = false
 			def linkOptions = attrs.linkOptions ?: [:]
 			if(item.urlName) {
-				linkOptions += [controller: "page", action: "show"]
+				linkOptions += [controller: "spudPage", action: "show"]
 				if(item.urlName != defaultPage) {
 					linkOptions.id = item.urlName
 				}
@@ -103,7 +103,7 @@ class SpudCmsTagLib {
 			def active = false
 			def linkOptions = attrs.linkOptions ?: [:]
 			if(item.urlName) {
-				linkOptions += [controller: "page", action: "show"]
+				linkOptions += [controller: "spudPage", action: "show"]
 				if(item.urlName != defaultPage) {
 					linkOptions.id = item.urlName
 				}
