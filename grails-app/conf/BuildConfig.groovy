75748e7f973774655a68b43da2419d6e528ec5b4
David Estes <davydotcom@gmail.com>
2014-06-06 09:03:20 -0400

    bumping permalink version to remove transactional overhead

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -28,7 +28,7 @@ grails.project.dependency.resolution = {
 
     plugins {
         runtime ":spud-core:0.4.2"
-        runtime ":spud-permalinks:0.4.0"
+        runtime ":spud-permalinks:0.4.1"
         runtime ':cache:1.1.1'
         runtime ':sitemaps:0.2.0'
 
