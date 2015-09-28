babd5de1c978de31c02d9a2231d755b35a9f4a16
David Estes <davydotcom@gmail.com>
2015-09-28 12:57:56 -0400

    version bump

--- SpudCmsGrailsPlugin.groovy
+++ SpudCmsGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class SpudCmsGrailsPlugin {
-    def version = "0.6.18"
+    def version = "0.6.19"
     def grailsVersion = "2.3 > *"
     def pluginExcludes = [
         "grails-app/views/error.gsp"
